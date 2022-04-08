bf72353e55c5d16132e711cb20ebc666298b2b45
akniputra <aknibroken212@gmail.com>
2022-04-08 15:40:57 +0700

    Percobaan Data Classes and Destructuring Declarations

---
+++ src/main/kotlin/id/ac/polbeng/akni/test_kelas/DataClassE.kt
@@ -0,0 +1,12 @@
+package id.ac.polbeng.akni.test_kelas
+
+//destructing declarations untuk mendestruksi data kelas sebuah objek kedalam sejumlah variabel
+
+fun main() {
+    val puteri = Student("Puteri", 21)
+
+    //Destructuring Declaration
+    val(name, age) = puteri
+    println("Name = $name")
+    println("Age = $age")
+}
