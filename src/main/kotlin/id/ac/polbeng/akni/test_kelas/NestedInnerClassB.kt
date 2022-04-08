7c765437e8ba2a0892c7b67f8cf3dae72f3af942
akniputra <aknibroken212@gmail.com>
2022-04-08 15:26:53 +0700

    Percobaan Anonymous Class

---
+++ src/main/kotlin/id/ac/polbeng/akni/test_kelas/NestedInnerClassB.kt
@@ -0,0 +1,18 @@
+package id.ac.polbeng.akni.test_kelas
+
+//anonymous class adalah konsep membuat sebuah objek interface menggunakan objek runtime
+
+fun main() {
+    //membuat instance interface
+    var programmer: Human = object : Human {
+        override fun think() { //mengoverride method think()
+            print("I am an example of Anonymous Inner Class ")
+        }
+    }
+    programmer.think()
+}
+
+//interface Human
+interface Human {
+    fun think()
+}
